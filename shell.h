fc227b499b27570e96ed2ebc038af83edfbd94db
Emmanuel Ozioko <emmanuelozioko35@gmail.com>
2022-12-21 22:30:55 +0000

    correct function

--- shell.h
+++ shell.h
@@ -160,25 +160,25 @@ void *_realloc(void *, unsigned int, unsigned int);
 /* memory_functions2.c */
 int bfree(void **);
 
-/* more_functions.c */
+/* extra_functions.c */
 int interactive(info_t *);
 int is_delim(char, char *);
 int _isalpha(int);
 int _atoi(char *);
 
-/* more_functions2.c */
+/* extra_functions2.c */
 int _erratoi(char *);
 void print_error(info_t *, char *);
 int print_d(int, int);
 char *convert_number(long int, int, int);
 void remove_comments(char *);
 
-/* builtin_emulators.c */
+/* built-in_emulators.c */
 int _myexit(info_t *);
 int _mycd(info_t *);
 int _myhelp(info_t *);
 
-/* builtin_emulators2.c */
+/* built-in_emulators2.c */
 int _myhistory(info_t *);
 int _myalias(info_t *);
 
@@ -192,14 +192,14 @@ void clear_info(info_t *);
 void set_info(info_t *, char **);
 void free_info(info_t *, int);
 
-/* env.c module */
+/* environs.c module */
 char *_getenv(info_t *, const char *);
 int _myenv(info_t *);
 int _mysetenv(info_t *);
 int _myunsetenv(info_t *);
 int populate_env_list(info_t *);
 
-/* env2.c module */
+/* environs2.c module */
 char **get_environ(info_t *);
 int _unsetenv(info_t *, char *);
 int _setenv(info_t *, char *, char *);
@@ -211,14 +211,14 @@ int read_history(info_t *info);
 int build_history_list(info_t *info, char *buf, int linecount);
 int renumber_history(info_t *info);
 
-/* liststr.c module */
+/* liststring.c module */
 list_t *add_node(list_t **, const char *, int);
 list_t *add_node_end(list_t **, const char *, int);
 size_t print_list_str(const list_t *);
 int delete_node_at_index(list_t **, unsigned int);
 void free_list(list_t **);
 
-/* liststr2.c module */
+/* liststring2.c module */
 size_t list_len(const list_t *);
 char **list_to_strings(list_t *);
 size_t print_list(const list_t *);
